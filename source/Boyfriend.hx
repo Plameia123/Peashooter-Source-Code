f52338050db9313b746ec05467287f07501383ac
joaod <joaodanielruasvieira17@gmail.com git config user.namegit config --global user.email joaodanielruasvieira17@gmail.com>
2022-09-29 14:54:59 -0300

    Publishing Stuff

---
+++ source/Boyfriend.hx
@@ -0,0 +1,51 @@
+package;
+
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.graphics.frames.FlxAtlasFrames;
+import flixel.util.FlxTimer;
+
+using StringTools;
+
+class Boyfriend extends Character
+{
+	public var stunned:Bool = false;
+
+	public function new(x:Float, y:Float, ?char:String = 'bf')
+	{
+		switch (char)
+		{
+			case 'peashooter':
+				super(x, y - 90, char, true);
+			case 'peashooter-angey':
+				super(x, y - 50, char, true);
+			default:
+				super(x, y, char, true);
+		}
+	}
+
+	override function update(elapsed:Float)
+	{
+		if (!debugMode)
+		{
+			if (animation.curAnim.name.startsWith('sing'))
+			{
+				holdTimer += elapsed;
+			}
+			else
+				holdTimer = 0;
+
+			if (animation.curAnim.name.endsWith('miss') && animation.curAnim.finished && !debugMode)
+			{
+				playAnim('idle', true, false, 10);
+			}
+
+			if (animation.curAnim.name == 'firstDeath' && animation.curAnim.finished)
+			{
+				playAnim('deathLoop');
+			}
+		}
+
+		super.update(elapsed);
+	}
+}
